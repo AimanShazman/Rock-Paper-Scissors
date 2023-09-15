d5f5904b3f6feebf094d7fa0ec71999f1c0a8a4d
AimanShazman <136099677+CrusineZ@users.noreply.github.com>
2023-09-15 21:41:16 +0800

    add playRound() and display win/lose
    compare the player and computer selections. Display the result in
    console.

--- myscripts.js
+++ myscripts.js
@@ -13,6 +13,33 @@ function getComputerChoice() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    //tie
+    if (playerSelection === computerSelection) {
+        return "It's a Tie!\n";
+    } 
+
+    if(playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            return "You Win! Rock beats Scissors\n"
+        } else {
+            return "You Lose! Paper beats Rock\n";
+        }
+    } else if (playerSelection === 'paper') { 
+        if (computerSelection === 'rock') {
+            return "You Win! Paper beats Rock\n"
+        } else {
+            return "You Lose! Scissors beats Paper\n";
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return "You Win! Scissors beats Paper\n"
+        } else {
+            return "You Lose! Rock beats Scissors\n";
+        }
+    }
+}
+
 //get user choice (Rock/Paper/Scissors)
 let playerSelection = prompt("Choose either Rock, Paper or Scissors: ").toLowerCase();
 if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
@@ -26,5 +53,6 @@ if (!computerSelection) {
 }
 
 //play round
+console.log(playRound(playerSelection, computerSelection));
 
-//display marks
+
