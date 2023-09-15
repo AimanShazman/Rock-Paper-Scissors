830b5d6112badcfe68901f68b3feec46c5419bb1
AimanShazman <136099677+CrusineZ@users.noreply.github.com>
2023-09-15 21:10:17 +0800

    identify comp selection
    identify computer selection with if statement. If 1: return rock, if 2:
    return paper, if 3: return scissors.

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,27 @@
 //get computer choice (Rock/Paper/Scissors)
 function getComputerChoice() {
     let computerSelection = Math.ceil(Math.random()*3);
-    console.log(computerSelection);
+
+    if (computerSelection === 1) {
+        return 'rock';
+    } else if (computerSelection === 2) {
+        return 'paper';
+    } else if (computerSelection === 3) {
+        return 'scissors';
+    } else {
+        return false;
+    }
 }
 
 //get user choice (Rock/Paper/Scissors)
 let playerSelection = prompt("Choose either Rock, Paper or Scissors: ").toLowerCase();
+
+
+//get computer choice
+let computerSelection = getComputerChoice();
+if (!computerSelection) {
+    Error('Cannot get computer selection!\n');
+}
 
 //play round
 
