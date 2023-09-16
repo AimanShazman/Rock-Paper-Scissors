e5ab76065636ac3984ea84907e1eade76a40b6b6
AimanShazman <136099677+CrusineZ@users.noreply.github.com>
2023-09-16 17:49:06 +0800

    add game(). Move prompt into game()
    add game() to make it playable 5 rounds, display the result for each
    round, and display the score after 5 rounds. move the user and comp
    selection into game().

--- myscripts.js
+++ myscripts.js
@@ -13,46 +13,82 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, result) {
     //tie
     if (playerSelection === computerSelection) {
-        return "It's a Tie!\n";
-    } 
-
-    if(playerSelection === 'rock') {
+        result =  "It's a Tie!\n";
+        result = 'tie';
+    } else if(playerSelection === 'rock') {
         if (computerSelection === 'scissors') {
-            return "You Win! Rock beats Scissors\n"
+            // result =  "You Win! Rock beats Scissors\n"
+            result = 'player';
         } else {
-            return "You Lose! Paper beats Rock\n";
+            // result =  "You Lose! Rock is defeated by Paper\n";
+            result = 'comp';
         }
     } else if (playerSelection === 'paper') { 
         if (computerSelection === 'rock') {
-            return "You Win! Paper beats Rock\n"
+            // result =  "You Win! Paper beats Rock\n"
+            result = 'player';
         } else {
-            return "You Lose! Scissors beats Paper\n";
+            // result =  "You Lose! Paper is defeated by Scissors\n";
+            result = 'comp';
         }
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'paper') {
-            return "You Win! Scissors beats Paper\n"
+            // result =  "You Win! Scissors beats Paper\n"
+            result = 'player';
         } else {
-            return "You Lose! Rock beats Scissors\n";
+            // result =  "You Lose! Scissors is defeated by Rock\n";
+            result = 'comp';
         }
     }
+    return result;
 }
 
-//get user choice (Rock/Paper/Scissors)
-let playerSelection = prompt("Choose either Rock, Paper or Scissors: ").toLowerCase();
-if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-    throw Error('Invalid Input!\n');
+function game() {
+    let playerSelection;
+    let computerSelection;
+    let playerPoints = 0;
+    let compPoints = 0;
+    let temp;
+
+    for (let i = 0; i < 5; i++) {
+        //get user choice (Rock/Paper/Scissors)
+        playerSelection = prompt("Choose either Rock, Paper or Scissors: ").toLowerCase();
+        if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+            throw Error('Invalid Input!\n');
+        }
+
+        //get computer choice
+        computerSelection = getComputerChoice();
+        if (!computerSelection) {
+            throw Error('Cannot get computer selection!\n');
+        }       
+
+        temp = playRound(playerSelection, computerSelection);
+
+        //add points 
+        if (temp === 'tie') {
+            console.log("It's a Tie!");
+        } else if (temp === 'player') {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}\n`);
+            playerPoints++;
+        } else if (temp === 'comp') {
+            console.log(`You Lose! ${playerSelection} is defeated by ${computerSelection}\n`);
+            compPoints++;
+        }
+    }
+
+    //after 5 times, display winner
+    if (playerPoints === compPoints) {
+        console.log("It's a Tie!");
+    } else if (playerPoints > compPoints) {
+        console.log(`Game Ended: You Win! ${playerPoints}-${compPoints}`);
+    } else if (compPoints > playerPoints) {
+        console.log(`Game Ended: You Lose! ${playerPoints}-${compPoints}`);
+    }
+    return;
 }
 
-//get computer choice
-let computerSelection = getComputerChoice();
-if (!computerSelection) {
-    throw Error('Cannot get computer selection!\n');
-}
-
-//play round
-console.log(playRound(playerSelection, computerSelection));
-
-
+game();
